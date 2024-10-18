6728db752e69f127f86619baf7ff227c6d099147
marianamade <marianamaderag@gmail.com>
2024-10-18 07:52:07 -0500

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,27 @@
+#asigna un valor a una variable.
+x = 5
+#suma y asigna.
+#es lo mismo que x = x + 3
+x += 3
+#resta y asigna.
+x -= 2  # Es lo mismo que x = x - 2
+#multiplica y asigna.
+x *= 4  # Es lo mismo que x = x * 4
+#divide y asigna.
+x /= 5  # Es lo mismo que x = x / 5
+#modulo y asigna.
+x %= 2  # Es lo mismo que x = x % 2
+#division entera y asigna.
+x //= 3  # Es lo mismo que x = x // 3
+#exponente y asigna.
+x **= 2  # Es lo mismo que x = x ** 2
+#asignacion bit a bit
+x &= 3  # Es lo mismo que x = x & 3
+# asignacion OR bit a bit
+x |= 2  # Es lo mismo que x = x | 2
+#asignacion XOR bit a bit
+x ^= 3  # Es lo mismo que x = x ^ 3
+# Desplazamiento a la derecha y asigna.
+x >>= 1  # Es lo mismo que x = x >> 1
+#Desplazamiento a la izquierda y asigna.
+x <<= 2  # Es lo mismo que x = x << 2
